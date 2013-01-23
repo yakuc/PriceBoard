9a7a4325890960865c3ea8b8b1de9ec2d021fc5c
YAKUC <noyaku51@gmail.com>
2013-01-24 07:16:15 +0900

    最初のソースコミット。

---
+++ test/com/yakuc/priceboard/driver/GaitameDotComDriverTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.yakuc.priceboard.driver;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nobu
+ */
+public class GaitameDotComDriverTest {
+
+    public GaitameDotComDriverTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+//        GaitameDotComDriver driver = new GaitameDotComDriver();
+        
+    }
+
+}
